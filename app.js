17fb36cb20ddb9687803a443e36e08c5b16c8d7f
Ryan8765 <rmhaas2211@gmail.com>
2015-01-14 14:37:49 -0600

    Updated Nav

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
 //--------------------------------
+	//create Ajax call for Instagram
+	function instagramAjax () {
+		$.ajax ({
+			url: "https://api.instagram.com/v1/users/3/media/recent/?client_id=f32e7af0d6d941009db0a4406adb4e88",
+			dataType: "jsonp",
+			success: function(data) {
+				console.log(data);
+			}//end success
+		});//end ajax request
+	}//end instagram function
 
-
-
+	instagramAjax();
 	
 //----------------------------------	
 });
